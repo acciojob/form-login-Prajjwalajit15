bd1eacb9c27b21b26f2433072dfcb9badd1c897e
prajjwal ajit <122018880+Prajjwalajit15@users.noreply.github.com>
2023-09-16 13:27:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 function getFormvalue() {
-    //Write your code here
+    // Get the form element by its ID
+    var form = document.getElementById("form1");
 
+    // Get the values of the first and last name fields
+    var firstName = form.elements.fname.value;
+    var lastName = form.elements.lname.value;
+
+    // Check if both fields are filled out
+    if (firstName && lastName) {
+        // Display the values using an alert
+        alert(firstName +" "+lastName);
+    } else {
+        alert("Please fill out both First name and Last name fields.");
+    }
+
+    // Prevent the form from submitting and refreshing the page
+    return false;
 }
